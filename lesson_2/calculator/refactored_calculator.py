6ccb1eea0a4ab9874fab90e21325a9a6716efeec
AngelQubits <156223103+AngelQubits@users.noreply.github.com>
2024-05-29 14:09:30 +0000

    added bonus feature:  prompting the user to request another calculation

--- lesson_2/calculator/refactored_calculator.py
+++ lesson_2/calculator/refactored_calculator.py
@@ -17,44 +17,64 @@ def is_invalid(number_str):
         return True
     return False
 
+def calc_core():
+   '''This function is the core program of the calculator and can be run
+   again by invoking it'''
+
+   prompt('Please enter your first number: ')
+   number1 = input()
+   print()
+   while is_invalid(number1):
+       prompt('Please Enter A Valid Number: ')
+       number1 = input()
+   
+   prompt('Thank You! Please enter your second number: ')
+   number2 = input()
+   print()
+   
+   while is_invalid(number2):
+       prompt('Please Enter A Valid Number: ')
+       number2 = input()
+   
+   prompt("Please select the number of the operation you'd like to perform: \n"
+   "1) Add \n"
+   "2) Subtract \n"
+   "3) Divide \n"
+   "4) Multiply\n")
+   operation = input()
+   print()
+   
+   while operation not in ['1', '2', '3', '4']:
+        prompt('Please Enter A Valid Number [1, 2, 3, 4]')
+        operation = input()
+   match operation:
+       case '1':
+           output = int(number1) + int(number2)
+       case '2':
+           output = int(number1) - int(number2)
+       case '3':
+           output = int(number1) / int(number2)
+       case '4':
+           output = int(number1) * int(number2)
+   prompt(f'Your result is: {output} \n')
+
+
 prompt('Welcome to Calculator!')
-prompt('Please enter your first number: ')
+calc_core()
 
-number1 = input()
-print()
-
-while is_invalid(number1):
-    prompt('Please Enter A Valid Number: ')
-    number1 = input()
-
-prompt('Thank You! Please enter your second number: ')
-number2 = input()
-print()
-
-while is_invalid(number2):
-    prompt('Please Enter A Valid Number: ')
-    number2 = input()
-
-prompt("Please select the number of the operation you'd like to perform: \n"
-"1) Add \n"
-"2) Subtract \n"
-"3) Divide \n"
-"4) Multiply\n")
-operation = input()
-print()
-
-while operation not in ['1', '2', '3', '4']:
-    prompt('Please Enter A Valid Number [1, 2, 3, 4]')
-    operation = input()
-
-match operation:
-    case '1':
-        output = int(number1) + int(number2)
-    case '2':
-        output = int(number1) - int(number2)
-    case '3':
-        output = int(number1) / int(number2)
-    case '4':
-        output = int(number1) * int(number2)
-prompt(f'Your result is: {output} \n')
-print()
+while True:
+ prompt(f'''You have reached the end of our program.  Would you like to start again?
+ Enter '1' for Yes
+ Enter '2' for No''')
+ again = input()
+ if again == '2':
+  prompt('Thank You! Our program has now concluded. Good Bye!')
+  break
+ if again == '1':
+  calc_core()
+  continue
+ else:
+  prompt('''Please Enter a Valid Entry.
+  Enter '1' for Yes
+  Enter '2' for No''')
+  again = input()
