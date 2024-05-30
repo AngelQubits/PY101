290ddd2076bdf7caca90b90961b7f0cd563f5f9c
AngelQubits <156223103+AngelQubits@users.noreply.github.com>
2024-05-30 10:17:42 +0000

    added bonus feature: initial configuration json file

--- lesson_2/calculator/refactored_calculator.py
+++ lesson_2/calculator/refactored_calculator.py
@@ -21,31 +21,27 @@ def calc_core():
    '''This function is the core program of the calculator and can be run
    again by invoking it'''
 
-   prompt('Please enter your first number: ')
+   prompt(data["message1"])
    number1 = input()
    print()
    while is_invalid(number1):
-       prompt('Please Enter A Valid Number: ')
+       prompt(data["message2"])
        number1 = input()
    
-   prompt('Thank You! Please enter your second number: ')
+   prompt(data["message3"])
    number2 = input()
    print()
    
    while is_invalid(number2):
-       prompt('Please Enter A Valid Number: ')
+       prompt(data["message2"])
        number2 = input()
    
-   prompt("Please select the number of the operation you'd like to perform: \n"
-   "1) Add \n"
-   "2) Subtract \n"
-   "3) Divide \n"
-   "4) Multiply\n")
+   prompt(data["message4"])
    operation = input()
    print()
    
    while operation not in ['1', '2', '3', '4']:
-        prompt('Please Enter A Valid Number [1, 2, 3, 4]')
+        prompt(data["message5"])
         operation = input()
    match operation:
        case '1':
@@ -58,23 +54,22 @@ def calc_core():
            output = int(number1) * int(number2)
    prompt(f'Your result is: {output} \n')
 
-
-prompt('Welcome to Calculator!')
+import json
+with open('calculator_messages.json', 'r') as file:
+    data = json.load(file)
+ 
+prompt(data["welcome"])
 calc_core()
 
 while True:
- prompt(f'''You have reached the end of our program.  Would you like to start again?
- Enter '1' for Yes
- Enter '2' for No''')
+ prompt(data["message7"])
  again = input()
  if again == '2':
-  prompt('Thank You! Our program has now concluded. Good Bye!')
+  prompt(data["message8"])
   break
  if again == '1':
   calc_core()
   continue
  else:
-  prompt('''Please Enter a Valid Entry.
-  Enter '1' for Yes
-  Enter '2' for No''')
+  prompt(data["message9"])
   again = input()
