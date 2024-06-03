c989ea0e90f6fdcc704cbcb67db940e92c1cf5cc
AngelQubits <156223103+AngelQubits@users.noreply.github.com>
2024-06-03 09:05:46 +0000

    added edge cases: negative price values, data validation, numerical formating

--- lesson_2/car_calc/car_calc.py
+++ lesson_2/car_calc/car_calc.py
@@ -1,23 +1,50 @@
 print("Welcome to Our Car Loan Calculator!")
 print("Please Enter Your Loan Amount: ")
-loan_amt = float(input())
-# if loan_amt <= 0:
-# 	print("Invalid Entry. Please enter value greater than 1.")
+while True:
+    try:
+	    loan_amt = float(input())
+	    if loan_amt <= 499:
+		    print("Invalid Entry. Please enter value greater than $500 "
+		    + "(Only Numbers): ")
+	    else:
+		    break
+    except ValueError:
+        print("Please Enter a Valid Number Greater Than $500: ")
 
 print()
 print("Thank You! Please Enter Your Interest Rate: ")
-monthly_rate = (float(input()) / 100) / 12
-#print("$", loan_amt, "This is your monthly rate: ", monthly_rate, "%")
+while True:
+	try:
+		monthly_rate = (float(input()) / 100) / 12
+		if monthly_rate < 0:
+			print("Invalid Entry. Please Enter Value Greater Than 0 " +
+			    "(i.e. 4.45 for 4.45%): ")
+		else:
+		    break
+	except ValueError:
+		print("Invalid Entry. Please Enter Value Greater Than 0 " +
+			    "(i.e. 4.45 for 4.45%): ")
+	    
 
 print()
 print("Now Enter Loan Term in Years (1-7): \n")
-monthly_duration = int(input()) * 12
+while True:
+	try:
+		monthly_duration = int(input()) * 12
+		if monthly_duration not in [12, 24, 36, 48, 60, 72, 84]:
+			print("Invalid Entry. Please Enter a Valid Number From 1-7: ")
+		else:
+			break
+	except ValueError:
+		print("Invalid Entry. Please Enter a Valid Number From 1-7: ")
+		
 
 def monthly_payment():
 	mp = round(loan_amt * (monthly_rate / (1 - (1 + monthly_rate) ** (-monthly_duration))), 2)
 	monthly_payment = '{:,}'.format(mp)
 	res = round(mp * monthly_duration, 2)
 	output = '{:,}'.format(res)
+	print()
 	print("*********************\nYour Monthly Payment Will Be: $", monthly_payment)
 	print("You'll have a total of ", monthly_duration, "payments.")
 	print("You will have paid a total amount of $" + output + 
